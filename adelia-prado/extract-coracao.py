f4a3727103e08719f078a5076104b23cfa66dfb3
Felipe Marinho <felipevm97@gmail.com>
2019-06-08 14:12:10 -0300

    o coraçao disparado

---
+++ adelia-prado/extract-coracao.py
@@ -0,0 +1,37 @@
+import ebooklib
+from ebooklib import epub
+from bs4 import BeautifulSoup
+
+book = epub.read_epub('O Coracao Disparado - Adelia Prado.epub')
+count = 0
+raw = []
+poemas = {}
+ignoreNext = False  # para ignorar os epígrafes após início da cada capítulo
+
+for item in book.get_items():
+    if item.get_type() == ebooklib.ITEM_DOCUMENT:
+        if count > 3 and not ignoreNext:
+            soup = BeautifulSoup(item.get_content(), 'html.parser')
+            extract = soup.get_text().strip().split('\n')
+            raw.append(soup.get_text().strip())
+            titulo = extract[0]
+            corpo = extract[1:]
+            # if len(corpo) == 0 and not titulo == 'ALFÂNDEGA':
+            #     ignoreNext = True
+            # elif titulo == 'Alfândega':
+            #     ignoreNext = True
+            poemas[titulo] = corpo
+        elif ignoreNext:
+            ignoreNext = False
+        count += 1
+
+file = open('coracao-disparado.txt', 'w')
+
+dataset = ''
+
+for titulo in poemas.keys():
+    dataset += titulo.upper() + '\n\n'
+    dataset += '\n'.join(poemas[titulo]) + '\n\n'
+
+file.write(dataset)
+# print(poemas)
